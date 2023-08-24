bcc06723a4c55aad4f2a487c0f83dc64816f556f
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-08-24 20:14:10 -0300

    Fixed pagination utility

--- src/backend/nekosauce/sauces/utils/__init__.py
+++ src/backend/nekosauce/sauces/utils/__init__.py
@@ -1,2 +1,2 @@
 def paginate(iterable, limit: int) -> list:
-    return [(iterable[i : i + limit], i) for i in range(0, len(iterable), limit)]
+    return [iterable[i : i + limit] for i in range(0, len(iterable), limit)]
