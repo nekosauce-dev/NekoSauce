d106e121bbbbc1fee111ca4027070ff62a2dd226
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-08-24 15:43:48 -0300

    Made celery cron tasks configurable from the admin site

--- src/backend/nekosauce/sauces/tasks.py
+++ src/backend/nekosauce/sauces/tasks.py
@@ -14,12 +14,12 @@ def calc_hashes(sauce_id: int, img_bytes: bytes, replace: bool = True):
 
 
 @shared_task
-def update_sauces():
+def update_sauces(async_reqs=3, chunk_size=1024):
     from nekosauce.sauces.management.commands.updatesauces import Command
-    Command().handle(async_reqs=3, chunk_size=1024)
+    Command().handle(async_reqs=async_reqs, chunk_size=chunk_size)
 
 
 @shared_task
-def update_hashes():
+def update_hashes(limit=10000, async_reqs=3, chunk_size=128):
     from nekosauce.sauces.management.commands.updatehashes import Command
-    Command().handle(limit=10000, async_reqs=3, chunk_size=128)
+    Command().handle(limit=limit, async_reqs=async_reqs, chunk_size=chunk_size)
