0bc2f62e2afcaef8b0bf3e197b1276dae621a12a
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-08-25 14:55:26 -0300

    Updated celery config

--- src/backend/nekosauce/settings.py
+++ src/backend/nekosauce/settings.py
@@ -149,13 +149,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Celery Configuration Options
 CELERY_BROKER_URL = "amqp://nekosauce:nekosauce@" + ("rabbitmq" if not DEBUG else "localhost") + ":5672/"
-CELERY_RESULT_BACKEND = "rpc://nekosauce:nekosauce@" + ("rabbitmq" if not DEBUG else "localhost") + ":5672/"
+# CELERY_RESULT_BACKEND = "rpc://nekosauce:nekosauce@" + ("rabbitmq" if not DEBUG else "localhost") + ":5672/"
 CELERY_TIMEZONE = "America/Argentina/Buenos_Aires"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_TASK_ROUTES = {
     "sauces.tasks.calc_hashes": {"queue": "default"},
 }
+CELERY_TASK_ACKS_LATE = True
 
 
 REST_FRAMEWORK = {
