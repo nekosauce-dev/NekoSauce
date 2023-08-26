d4f606d9d6488e409df396ec888713ba5a3426ec
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-08-26 15:51:32 -0300

    Create multiple celery workers instead of setting concurrency

--- src/backend/run.sh
+++ src/backend/run.sh
@@ -3,7 +3,7 @@
 # Function to clean up and stop all processes
 cleanup_and_stop() {
     echo "Stopping all processes..."
-    kill -TERM $gunicorn_pid $celery_worker_pid $celery_beat_pid
+    kill -TERM $gunicorn_pid $celery_worker_pids $celery_beat_pid
 }
 
 # Trap the exit signals and call the cleanup function
@@ -15,13 +15,16 @@ cd /app
 # Apply Django migrations
 python manage.py migrate
 
-# Start Gunicorn with specified workers and binding address
+# Start Gunicorn with multiple workers and binding address
 gunicorn -w $GUNICORN_WORKERS -b 0.0.0.0:8000 nekosauce.wsgi:application &
 gunicorn_pid=$!
 
-# Start Celery worker with gevent pool and concurrency of 4
-celery -A nekosauce worker -l INFO -P gevent -c $CELERY_WORKERS &
-celery_worker_pid=$!
+# Start multiple Celery workers with gevent pool and concurrency of 1
+celery_worker_pids=()
+for ((i=1; i<=$CELERY_WORKERS; i++)); do
+    celery -A nekosauce worker -l INFO -P gevent -c 1 -n worker$i@nekosauce.org &
+    celery_worker_pids+=($!)
+done
 
 # Start Celery beat scheduler with DatabaseScheduler
 celery -A nekosauce beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
