70615698086374b2ffbfeebaa6b60acc7a081034
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-08-27 22:21:13 -0300

    Migrated backend error handlers to Django's HttpResponse

--- src/backend/nekosauce/views.py
+++ src/backend/nekosauce/views.py
@@ -1,3 +1,7 @@
+import json
+
+from django.http.response import HttpResponse
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -5,60 +9,68 @@ from nekosauce.exceptions import NotFound, Forbidden, ServerInternalError
 
 
 def error_400(request, exception):
-    return Response(
-        {
-            "errors": [
-                {
-                    "status": "bad_request",
-                    "message": "Seems like a value entered is not correct. Check your GET/POST params and try again!",
-                    "code": 400,
-                }
-            ]
-        },
+    return HttpResponse(
+        json.dumps(
+            {
+                "errors": [
+                    {
+                        "status": "bad_request",
+                        "message": "Seems like a value entered is not correct. Check your GET/POST params and try again!",
+                        "code": 400,
+                    }
+                ]
+            }
+        ),
         status=400,
     )
 
 
 def error_403(request, exception):
-    return Response(
-        {
-            "errors": [
-                {
-                    "status": "forbidden",
-                    "message": "I got you! Are you sure you have permission to go there? Something is telling me you're not allowed... >:)",
-                    "code": 403,
-                },
-            ]
-        },
+    return HttpResponse(
+        json.dumps(
+            {
+                "errors": [
+                    {
+                        "status": "forbidden",
+                        "message": "I got you! Are you sure you have permission to go there? Something is telling me you're not allowed... >:)",
+                        "code": 403,
+                    },
+                ]
+            }
+        ),
         status=403,
     )
 
 
 def error_404(request, exception):
-    return Response(
-        {
-            "errors": [
-                {
-                    "status": "not_found",
-                    "message": "Welcome dev kitten! Looking for some sauces? Then you may want to read the docs at `https://docs.nekosauce.org/` :)",
-                    "code": 404,
-                },
-            ],
-        },
+    return HttpResponse(
+        json.dumps(
+            {
+                "errors": [
+                    {
+                        "status": "not_found",
+                        "message": "Welcome dev kitten! Looking for some sauces? Then you may want to read the docs at `https://docs.nekosauce.org/` :)",
+                        "code": 404,
+                    },
+                ],
+            }
+        ),
         status=404,
     )
 
 
 def error_500(request):
-    return Response(
-        {
-            "errors": [
-                {
-                    "status": "server_error",
-                    "message": "Oopsie! Seems like something went wrong on our side :/ Maybe you want to report this error in our Discord server?",
-                    "code": 500,
-                }
-            ],
-        },
+    return HttpResponse(
+        json.dumps(
+            {
+                "errors": [
+                    {
+                        "status": "server_error",
+                        "message": "Oopsie! Seems like something went wrong on our side :/ Maybe you want to report this error in our Discord server?",
+                        "code": 500,
+                    }
+                ],
+            }
+        ),
         status=500,
     )
