2f73e9f31d0d6bcdd4e4eaec117884d8f79785c4
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-08-27 21:29:16 -0300

    Fixed backend error handlers

--- src/backend/nekosauce/views.py
+++ src/backend/nekosauce/views.py
@@ -4,25 +4,25 @@ from rest_framework.response import Response
 from nekosauce.exceptions import NotFound, Forbidden, ServerInternalError
 
 
-def error_400(request, exception):
+def error_400(request):
     raise ValidationError(
         "Seems like a value entered is not correct. Check your GET/POST params and try again!"
     )
 
 
-def error_403(request, exception):
+def error_403(request):
     raise Forbidden(
         "I got you! Are you sure you have permission to go there? Something is telling me you're not allowed... >:)"
     )
 
 
-def error_404(request, exception):
+def error_404(request):
     raise NotFound(
         "Welcome dev kitten! Looking for some sauces? Then you may want to read the docs at `https://docs.nekosauce.org/` :)"
     )
 
 
-def error_500(request, exception):
+def error_500(request):
     raise ServerInternalError(
         "Oopsie! Seems like something went wrong on our side :/ Maybe you want to report this error in our Discord server?"
     )
