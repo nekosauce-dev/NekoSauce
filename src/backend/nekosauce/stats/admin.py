b534cee432522d109af91d465f1bd7615032abbc
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-08-30 22:22:00 -0300

    Added `date_hierarchy` to `StatisticAdmin`

--- src/backend/nekosauce/stats/admin.py
+++ src/backend/nekosauce/stats/admin.py
@@ -9,6 +9,7 @@ class StatisticAdmin(admin.ModelAdmin):
     list_display = ("resource", "attribute", "value", "created_at")
     search_fields = ("resource", "attribute")
     list_filter = ("created_at",)
+    date_hierarchy = "created_at"
 
 
 admin.site.register(Statistic, StatisticAdmin)
