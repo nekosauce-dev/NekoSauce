63b7307d3d0a6f84e344a057df2fc522113f26f3
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-09-08 00:04:42 -0300

    Added user admin to admin site

--- src/backend/nekosauce/users/admin.py
+++ src/backend/nekosauce/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from nekosauce.users.models import *
 
 # Register your models here.
+
+
+admin.site.register(User, UserAdmin)
