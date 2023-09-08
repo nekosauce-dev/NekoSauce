8cba4e393ad6a951391e4396fd1543303501f345
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-09-08 00:04:53 -0300

    Added `api_key` property to users

--- src/backend/nekosauce/users/models.py
+++ src/backend/nekosauce/users/models.py
@@ -1,9 +1,17 @@
+import secrets
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
+def generate_api_key():
+    return secrets.token_urlsafe(64)
+
+
 class User(AbstractUser):
     donation = models.FloatField(default=0.0)
     donation_date = models.DateTimeField(null=True)
+
+    api_key = models.CharField(default=generate_api_key, max_length=86)
