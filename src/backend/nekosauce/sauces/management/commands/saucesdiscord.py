cdab71f5839fedc073421b8ebe3b1b8d30073062
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-09-25 20:08:02 -0300

    Created `saucesdiscord` backend management command

---
+++ src/backend/nekosauce/sauces/management/commands/saucesdiscord.py
@@ -0,0 +1,38 @@
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+
+import requests
+
+from nekosauce.sauces.models import Sauce, Hash, Source
+
+
+def format_large_number(number):
+    return '{:,}'.format(number)
+
+
+class Command(BaseCommand):
+    help = "Posts database update to Discord channel"
+
+    def handle(self, *args, **options):
+        new_line = "\n"
+        
+        self.stdout.write("Sending update (it can take a few seconds)...")
+
+        r = requests.post(
+            os.getenv("BACKEND_DISCORD_DATABASE_UPDATES_WEBHOOK_URL"),
+            json={
+                "content": (
+                    "__**NekoSauce Database Update!**__\n\n"
+                    f"Sauces: {format_large_number(Sauce.objects.count())}" + "\n"
+                    f"Hashes: {format_large_number(Hash.objects.count())}" + "\n\n"
+                    "**Which sources?**\n"
+                    f"{new_line.join([f'- {source.name}: {format_large_number(source.sauces.count())}' for source in Source.objects.all()])}"
+                    "\n\n------\n\n"
+                    "This update is automatic. NekoSauce will be released once the hashes amount matches (or almost matches) the amount of sauces."
+                )
+            },
+        )
+        r.raise_for_status()
+        
+        self.stdout.write("Update posted!")
