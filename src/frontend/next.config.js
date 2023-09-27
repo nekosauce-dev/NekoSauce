a45526d5a94d69eaaa719f779f0ddbc11bb86bac
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-09-27 03:29:45 -0300

    Updated `next.config.js` to set default env variables

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -2,11 +2,29 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        FOOTER_DISCORD_LINK: process.env.FRONTEND_FOOTER_DISCORD_LINK,
-        FOOTER_TWITTER_LINK: process.env.FRONTEND_FOOTER_TWITTER_LINK,
-        FOOTER_REDDIT_LINK: process.env.FRONTEND_FOOTER_REDDIT_LINK,
-        FOOTER_DONATE_LINK: process.env.FRONTEND_FOOTER_DONATE_LINK,
-        FUNDING_PROGRESS: process.env.FRONTEND_FUNDING_PROGRESS,
+        FOOTER_DISCORD_LINK:
+            process.env.FRONTEND_FOOTER_DISCORD_LINK &&
+            process.env.FRONTEND_FOOTER_DISCORD_LINK.length > 0
+                ? process.env.FRONTEND_FOOTER_DISCORD_LINK
+                : undefined,
+        FOOTER_TWITTER_LINK:
+            process.env.FRONTEND_FOOTER_TWITTER_LINK &&
+            process.env.FRONTEND_FOOTER_TWITTER_LINK.length > 0
+                ? process.env.FRONTEND_FOOTER_TWITTER_LINK
+                : undefined,
+        FOOTER_REDDIT_LINK:
+            process.env.FRONTEND_FOOTER_REDDIT_LINK &&
+            process.env.FRONTEND_FOOTER_REDDIT_LINK.length > 0
+                ? process.env.FRONTEND_FOOTER_REDDIT_LINK
+                : undefined,
+        FOOTER_DONATE_LINK:
+            process.env.FRONTEND_FOOTER_DONATE_LINK &&
+            process.env.FRONTEND_FOOTER_DONATE_LINK.length > 0
+                ? process.env.FRONTEND_FOOTER_DONATE_LINK
+                : undefined,
+        FUNDING_PROGRESS: process.env.FRONTEND_FUNDING_PROGRESS
+            ? process.env.FRONTEND_FUNDING_PROGRESS
+            : undefined,
         FUNDING_PROGRESS_FORMAT: process.env.FRONTEND_FUNDING_PROGRESS_FORMAT
             ? process.env.FRONTEND_FUNDING_PROGRESS_FORMAT
             : "{value}%",
