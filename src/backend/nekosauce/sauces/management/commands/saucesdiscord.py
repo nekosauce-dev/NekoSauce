cb7bb548529e63b00a11eba7a18671930a1eb00c
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-09-28 10:52:42 -0300

    Sort discord update sources by ID

--- src/backend/nekosauce/sauces/management/commands/saucesdiscord.py
+++ src/backend/nekosauce/sauces/management/commands/saucesdiscord.py
@@ -8,7 +8,7 @@ from nekosauce.sauces.models import Sauce, Hash, Source
 
 
 def format_large_number(number):
-    return '{:,}'.format(number)
+    return "{:,}".format(number)
 
 
 class Command(BaseCommand):
@@ -16,7 +16,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         new_line = "\n"
-        
+
         self.stdout.write("Sending update (it can take a few seconds)...")
 
         r = requests.post(
@@ -27,12 +27,12 @@ class Command(BaseCommand):
                     f"Sauces: {format_large_number(Sauce.objects.count())}" + "\n"
                     f"Hashes: {format_large_number(Hash.objects.count())}" + "\n\n"
                     "**Which sources?**\n"
-                    f"{new_line.join([f'- {source.name}: {format_large_number(source.sauces.count())}' for source in Source.objects.filter(enabled=True)])}"
+                    f"{new_line.join([f'- {source.name}: {format_large_number(source.sauces.count())}' for source in Source.objects.filter(enabled=True).order_by('id')])}"
                     "\n\n------\n\n"
                     "This update is automatic. NekoSauce will be released once the hashes amount matches (or almost matches) the amount of sauces."
                 )
             },
         )
         r.raise_for_status()
-        
+
         self.stdout.write("Update posted!")
