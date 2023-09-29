e4cc35522a94bd347e5eefcee374a8b65006117f
Nyeki.py <84998222+Nekidev@users.noreply.github.com>
2023-09-29 16:01:59 -0300

    Patched animepictures.py

--- src/backend/nekosauce/sauces/sources/animepictures.py
+++ src/backend/nekosauce/sauces/sources/animepictures.py
@@ -75,6 +75,9 @@ class AnimePicturesFetcher(sources.BaseFetcher):
 
         req_chunks = paginate(reqs, chunk_size)
 
+        if Len(req_chunks) == 0:
+            return
+
         while True:
             for index, response in grequests.imap_enumerated(
                 req_chunks[0],
