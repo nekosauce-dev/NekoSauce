5254ba11975a7763992a198b5f6a0305746b226f
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-10-13 23:10:49 -0300

    Update runbktree

--- src/backend/nekosauce/sauces/management/commands/runbktree.py
+++ src/backend/nekosauce/sauces/management/commands/runbktree.py
@@ -2,9 +2,13 @@ from django.core.management.base import BaseCommand, CommandError
 
 from fastapi import FastAPI
 
+from rich.progress import track
+
 import uvicorn
 
-from nekosauce.sauces import tree
+import pybktree
+
+from nekosauce.sauces.models import Sauce
 
 
 class Command(BaseCommand):
@@ -13,13 +17,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         app = FastAPI(title="NekoSauce BK Tree")
 
-        bktree = tree.load()
+        tree = pybktree.BKTree(lambda x, y: pybktree.hamming_distance(x[0], y[0]))
+
+        for sauce in track(
+            Sauce.objects.filter(status=Sauce.Status.PROCESSED),
+            "Adding hashes to tree...",
+        ):
+            tree.add(
+                (int(sauce.hash, 2), sauce.id),
+            )
 
         @app.get("/find")
         async def find(bits: str, distance: int):
             return [
                 {"d": item[0], "h": item[1][0], "id": item[1][1]}
-                for item in sorted(bktree.find((int(bits, 2), 0), distance))
+                for item in sorted(tree.find((int(bits, 2), 0), distance))
             ]
 
         uvicorn.run(app, host="0.0.0.0", port=7171)
